75d2a51071bafea7cee60cc9b169674d48353694
BugHunter-X <askzenai@gmail.com>
2024-12-21 15:28:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This bug occurs when using Expo CLI to build an Android APK.  The build process fails with an error related to the Android SDK build tools version.  Specifically, the error message might mention incompatibility between the Expo CLI's expected version and the version installed on your system. This often happens after updating the Android SDK or Expo CLI itself.
